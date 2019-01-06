a3b54ca8e354e08b767af95373127055cd215c32
Mark <mcamrob@gmail.com>
2019-01-06 12:42:51 -0500

    Changed Form1 to MainNinjaForm. Updated project file.

--- SudokuNinja/MainNinjaForm.cs
+++ SudokuNinja/MainNinjaForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SudokuNinja
 {
-	public partial class Form1 : Form
+	public partial class MainNinjaForm : Form
 	{
-		public Form1()
+		public MainNinjaForm()
 		{
 			InitializeComponent();
 		}
--- SudokuNinja/Program.cs
+++ SudokuNinja/Program.cs
@@ -16,7 +16,7 @@ namespace SudokuNinja
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new MainNinjaForm());
 		}
 	}
 }
