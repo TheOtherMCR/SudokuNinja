11ed55f66194d2de6813abcdc7de9536b8c92b63
Mark <mcamrob@gmail.com>
2019-01-20 12:00:35 -0500

    Ended up just changing the assembly info file.

--- SudokuNinja/Properties/AssemblyInfo.cs
+++ SudokuNinja/Properties/AssemblyInfo.cs
@@ -5,13 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SudokuNinja")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SudokuNinja")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("Sudoku Ninja")]
+[assembly: AssemblyDescription("Program to suck all the joy out of solving Sudoku puzzles.")]
+[assembly: AssemblyInformationalVersion("January 20, 2019. 11:56am")]
+[assembly: AssemblyCompany("Mark C. Roberts, Toronto")]
+[assembly: AssemblyProduct("Sudoku Ninja")]
+[assembly: AssemblyCopyright("Copyright © Mark C. Roberts 2018")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
